09570c303cea08bce2920c2fe8a489fe169827bc
ichisuke55 <campistatrequartista10@gmail.com>
2023-10-25 23:16:33 +0900

    chore: add endpoint for graphql

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
 import uvicorn
+import strawberry
 from fastapi import FastAPI
+from strawberry.asgi import GraphQL
+
+
+@strawberry.type
+class User:
+    name: str
+    note: str
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def user(self) -> User:
+        return User(name="hoge", note="fuga")
+
+schema = strawberry.Schema(query=Query)
+graphql_app = GraphQL(schema)
 
 app = FastAPI()
 
 @app.get('/')
 def get_hello():
     return {'message': 'Hello World'}
+
+app.add_route("/graphql", graphql_app)
 
 if __name__ == '__main__':
     uvicorn.run(app)
